456c94e7191bef632ace68260ce11b8092bd701e
sgabriel190 <strilciuc_gabriel98@yahoo.ro>
2020-07-19 13:36:48 +0300

    split GeneralController and add ActivitiesController

---
+++ DoFest/DoFest.API/Controllers/AuthenticationController.cs
@@ -0,0 +1,52 @@
+﻿using DoFest.Entities.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DoFest.API.Controllers
+{
+    [Route("api/v1/auth")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        // ****** Servicii folosite de catre controller ******
+        //.........
+        // TODO: adaugarea serviciilor
+
+
+        /// Constructorul public care va injecta serviciile necesare prin IoC
+        public AuthenticationController()
+        {
+            // TODO
+        }
+
+        // ****** Maparea metodelor HTTP ******
+
+
+        [HttpPost("/login")]
+        public IActionResult Login([FromBody] User userModel)
+        {
+            // TODO: adaugarea logicii business
+            // TODO: adaugarea sintaxei pentru async/await
+            return Ok("Message from Login." +
+                      "\n[route: POST /api/v1/auth/login]");
+        }
+
+        [HttpPost("/register")]
+        public IActionResult Register([FromBody] User userModel)
+        {
+            // TODO: adaugarea logicii business
+            // TODO: adaugarea sintaxei pentru async/await
+            return Ok("Message from Register." +
+                      "\n[route: GET /api/v1/auth/register]");
+        }
+
+        [HttpPut("/changepassword")]
+        public IActionResult ChangePassword()
+        {
+            // TODO: adaugarea logicii business
+            // TODO: adaugarea sintaxei pentru async/await
+            return Ok("Message from ChangePassword." +
+                      "\n[route: GET /api/v1/auth/changepassword]");
+        }
+
+    }
+}
---
+++ DoFest/DoFest.API/Controllers/BucketlistController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DoFest.API.Controllers
+{
+    [Route("api/v1/bucketlists")]
+    [ApiController]
+    public class BucketlistController : ControllerBase
+    {
+        // ****** Servicii folosite de catre controller ******
+        //.........
+        // TODO: adaugarea serviciilor
+
+
+        /// Constructorul public care va injecta serviciile necesare prin IoC
+        public BucketlistController()
+        {
+            // TODO
+        }
+
+        // ****** Maparea metodelor HTTP ******
+
+        /// <summary>
+        /// Metoda GetBucketLists expusa public pe web prin care se acceseaza toate BucketList-urile utilizatorilor.
+        /// </summary>
+        /// <returns> Un raspuns Http care semnaleaza o eroare sau statusul ok impreuna cu datele cerute prin request. </returns>
+        [HttpGet("")]
+        public IActionResult GetBucketLists()
+        {
+            // TODO: adaugarea logicii business
+            // TODO: adaugarea sintaxei pentru async/await
+            return Ok("Message from GetBucketLists." +
+                      "\n[route: GET /api/v1/bucketlists]");
+        }
+
+    }
+}
