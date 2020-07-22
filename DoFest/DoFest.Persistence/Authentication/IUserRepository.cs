f6ac2e51ab99f025c15220b33c026f56368e7457
sgabriel190 <strilciuc_gabriel98@yahoo.ro>
2020-07-22 17:19:49 +0300

    create UserRepository

---
+++ DoFest/DoFest.Persistence/Authentication/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+using DoFest.Entities.Authentication;
+
+namespace DoFest.Persistence.Authentication
+{
+    public interface IUserRepository: IRepository<User>
+    {
+        Task<User> GetUserById(Guid id);
+
+        Task AddUser(User userEntity);
+    }
+}
---
+++ DoFest/DoFest.Persistence/Authentication/UserRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using DoFest.Entities.Authentication;
+using Microsoft.EntityFrameworkCore;
+
+namespace DoFest.Persistence.Authentication
+{
+    public sealed class UserRepository: Repository<User>, IUserRepository
+    {
+        public UserRepository(DoFestContext context) : base(context)
+        {
+        }
+
+        /// <summary>
+        /// Se creeaza un query pentru a gasi userul cu id-ul dat ca parametru.
+        /// </summary>
+        /// <param name="id"> Id-ul userului cautat. </param>
+        /// <returns> Un task ce contine o entitate User.</returns>
+        public async Task<User> GetUserById(Guid id)
+            => await context.Users
+                .FirstAsync(user => user.Id == id);
+
+        /// <summary>
+        /// Se adauga un user nou in database.
+        /// </summary>
+        /// <param name="userEntity"> O entitate de tip User ce va fi inserata.</param>
+        /// <returns></returns>
+        public async Task AddUser(User userEntity)
+            => await context.Users.AddAsync(userEntity);
+    }
+}
