3cf3cf8f5d787a9354e75a70d80388cc8fb89abe
sgabriel190 <strilciuc_gabriel98@yahoo.ro>
2020-07-22 19:16:20 +0300

    update UserModel

--- DoFest/DoFest.Business/Models/Authentication/UserModel.cs
+++ DoFest/DoFest.Business/Models/Authentication/UserModel.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using DoFest.Entities.Activities.Content;
-using DoFest.Entities.Authentication.Notification;
-using DoFest.Entities.Lists;
 
 namespace DoFest.Business.Models.Authentication
 {
@@ -18,16 +14,5 @@ namespace DoFest.Business.Models.Authentication
 
         public Guid? StudentId { get; set; }
 
-        public ICollection<Photo> Photos { get; set; }
-
-        public ICollection<Comment> Comments { get; set; }
-
-        public ICollection<Rating> Ratings { get; set; }
-
-        public ICollection<Note> Notes { get; set; }
-
-        public BucketList BucketList { get; set; }
-
-        public ICollection<Notification> Notifications { get; set; }
     }
 }
