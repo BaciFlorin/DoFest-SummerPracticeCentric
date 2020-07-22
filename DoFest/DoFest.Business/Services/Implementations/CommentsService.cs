2ca6ebe5cb680ca3c2e24ccee8030350ff1e0c0c
sgabriel190 <strilciuc_gabriel98@yahoo.ro>
2020-07-22 14:23:55 +0300

    update CommentsService

--- DoFest/DoFest.Business/Services/Implementations/CommentsService.cs
+++ DoFest/DoFest.Business/Services/Implementations/CommentsService.cs
@@ -1,9 +1,26 @@
-﻿using DoFest.Business.Services.Interfaces;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DoFest.Business.Models.Content.Comment;
+using DoFest.Business.Services.Interfaces;
 
 namespace DoFest.Business.Services.Implementations
 {
     public sealed class CommentsService: ICommentsService
     {
+        public Task<IEnumerable<CommentModel>> GetComments(Guid activityId)
+        {
+            throw new NotImplementedException();
+        }
 
+        public Task AddComment(Guid activityId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteComment(Guid activityId)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- DoFest/DoFest.Business/Services/Interfaces/ICommentsService.cs
+++ DoFest/DoFest.Business/Services/Interfaces/ICommentsService.cs
@@ -1,7 +1,17 @@
-﻿namespace DoFest.Business.Services.Interfaces
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DoFest.Business.Models.Content.Comment;
+
+namespace DoFest.Business.Services.Interfaces
 {
     public interface ICommentsService
     {
+        Task<IEnumerable<CommentModel>> GetComments(Guid activityId);
 
+        Task AddComment(Guid activityId);
+
+        Task DeleteComment(Guid activityId);
     }
 }
