23b345839e002762b802f2ae3bba2fa5d2ffe193
sgabriel190 <strilciuc_gabriel98@yahoo.ro>
2020-07-22 14:49:30 +0300

    add AuthenticationService and update with comments

--- DoFest/DoFest.Business/Services/Interfaces/ICommentsService.cs
+++ DoFest/DoFest.Business/Services/Interfaces/ICommentsService.cs
@@ -1,17 +1,34 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using DoFest.Business.Models.Content.Comment;
 
 namespace DoFest.Business.Services.Interfaces
 {
+    /// <summary>
+    /// Definirea contractul folosit de catre un serviciu de comentarii.
+    /// </summary>
     public interface ICommentsService
     {
+        /// <summary>
+        /// Selecteaza si returneaza toate comentariile asociate unei activitati din baza de date.
+        /// </summary>
+        /// <param name="activityId"> Id-ul unei activitati. </param>
+        /// <returns> Un enumerabil ce contine toate comentariile asociate unei activitati. </returns>
         Task<IEnumerable<CommentModel>> GetComments(Guid activityId);
 
+        /// <summary>
+        /// Adauga un comentariu la o activitate specificata.
+        /// </summary>
+        /// <param name="activityId"> Id-ul unei activitati. </param>
+        /// <returns> ??? Un string ce semnifica succesul/esecul operatiei. </returns>
         Task AddComment(Guid activityId);
 
+        /// <summary>
+        /// Sterge un comentariu asociat unei activitati.
+        /// </summary>
+        /// <param name="activityId"> Id-ul unei activitati. </param>
+        /// <returns> ??? Un string ce semnifica succesul/esecul operatiei. </returns>
         Task DeleteComment(Guid activityId);
     }
 }
