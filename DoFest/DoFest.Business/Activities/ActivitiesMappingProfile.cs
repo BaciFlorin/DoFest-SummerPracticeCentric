7928c1473564eeffadd874477444071b956ee540
sgabriel190 <strilciuc_gabriel98@yahoo.ro>
2020-07-28 22:52:35 +0300

    Update mapping profile and newcommentmodel

--- DoFest/DoFest.Business/Activities/ActivitiesMappingProfile.cs
+++ DoFest/DoFest.Business/Activities/ActivitiesMappingProfile.cs
@@ -1,8 +1,13 @@
 ﻿using AutoMapper;
+using DoFest.Business.Activities.Models.Activity;
+using DoFest.Business.Activities.Models.BucketList;
 using DoFest.Business.Activities.Models.Content.Comment;
 using DoFest.Business.Activities.Models.Content.Photos;
 using DoFest.Business.Activities.Models.Content.Ratings;
+using DoFest.Entities.Activities;
 using DoFest.Entities.Activities.Content;
+using DoFest.Entities.Authentication;
+using DoFest.Entities.Lists;
 
 namespace DoFest.Business.Activities
 {
@@ -23,6 +28,11 @@ namespace DoFest.Business.Activities
             CreateMap<NewCommentModel, Comment>();
             CreateMap<CommentModel, Comment>();
             CreateMap<Comment, CommentModel>();
+
+            CreateMap<Activity, ActivityModel>();
+            CreateMap<BucketList, BucketListModel>();
+
+            CreateMap<User, BucketListModel>();
         }
     }
 }
--- DoFest/DoFest.Business/Activities/Models/Content/Comment/NewCommentModel.cs
+++ DoFest/DoFest.Business/Activities/Models/Content/Comment/NewCommentModel.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Text.Json.Serialization;
 
 namespace DoFest.Business.Activities.Models.Content.Comment
 {
     public sealed class NewCommentModel
     {
+        [JsonIgnore]
         public Guid UserId { get; set; }
         public string Content { get; set; }
     }
