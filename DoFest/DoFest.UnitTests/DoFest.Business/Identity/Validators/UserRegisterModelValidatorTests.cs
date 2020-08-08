63a33b69db2b2e498b8ebaa0f234e13bc5990c56
sgabriel190 <strilciuc_gabriel98@yahoo.ro>
2020-08-08 14:27:47 +0300

    Add factory and extension to unit testing.

--- DoFest/DoFest.UnitTests/DoFest.Business/Identity/Validators/UserRegisterModelValidatorTests.cs
+++ DoFest/DoFest.UnitTests/DoFest.Business/Identity/Validators/UserRegisterModelValidatorTests.cs
@@ -1,30 +1,46 @@
 ﻿using DoFest.Business.Identity.Validators;
-using DoFest.Business.Identity.Models;
 using Xunit;
-using System;
-using FluentValidation.TestHelper;
+using FluentAssertions;
+using DoFest.UnitTests.Shared.Factories;
 
 namespace DoFest.UnitTests.DoFest.Business.Identity.Validators
 {
     public class UserRegisterModelValidatorTests
     {
         [Fact]
-        public void InvalidPassword_RegisterModel_Test()
+        public void GivenValidate_WhenHavingAValidInput_ThenResultShouldBeValid()
         {
-            var model = new RegisterModel()
-            {
-                Email = "abc@test.test",
-                City = Guid.NewGuid(),
-                BucketListName = "12313",
-                Age = 20,
-                Year = 3,
-                Name = "Gigel Andrei",
-                Password = "sdadada",
-                Username = "gigel123"
-            };
+            var model = UserRegisterModelFactory.Default();
             var modelValidator = new UserRegisterModelValidator();
 
-            modelValidator.ShouldHaveValidationErrorFor( x => x.Password, model);
+            // modelValidator.ShouldHaveValidationErrorFor( x => x.Password, model);
+
+            var result = modelValidator.Validate(model);
+
+            result.IsValid.Should().BeTrue();
+            result.Errors.Count.Should().Be(0);
+        }
+
+        [Fact]
+        public void GivenValidate_WhenHavingANullEmail_ThenResultShouldBeInvalid()
+        {
+            var model = UserRegisterModelFactory.WithEmailNull();
+            var modelValidator = new UserRegisterModelValidator();
+
+            var result = modelValidator.Validate(model);
+
+            result.IsValid.Should().BeFalse();
+        }
+
+        [Fact]
+        public void GivenValidate_WhenHavingANullUsername_ThenResultShouldBeInvalid()
+        {
+            var model = UserRegisterModelFactory.WithUsernameNull();
+            var modelValidator = new UserRegisterModelValidator();
+
+            var result = modelValidator.Validate(model);
+
+            result.IsValid.Should().BeFalse();
         }
     }
 }
---
+++ DoFest/DoFest.UnitTests/Shared/Extensions/UserRegisterModelExtensions.cs
@@ -0,0 +1,20 @@
+﻿using DoFest.Business.Identity.Models;
+
+namespace DoFest.UnitTests.Shared.Extensions
+{
+    public static class UserRegisterModelExtensions
+    {
+        public static RegisterModel WithEmail(this RegisterModel model, string email)
+        {
+            model.Email = email;
+            return model;
+        }
+
+        public static RegisterModel WithUsername(this RegisterModel model, string username)
+        {
+            model.Username = username;
+            return model;
+        }
+    }
+}
+}
