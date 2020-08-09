4aa9a4bdefbdee39b78ec790d2bc2cf316ab3ef4
sgabriel190 <strilciuc_gabriel98@yahoo.ro>
2020-08-09 16:15:07 +0300

    Update unit tests

---
+++ DoFest/DoFest.UnitTests/DoFest.Business/Activities/Validators/Activity/ActivityType/ActivityTypeModelTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace DoFest.UnitTests.Activities.Activity.ActivityType
+{
+    public class ActivityTypeModelTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
