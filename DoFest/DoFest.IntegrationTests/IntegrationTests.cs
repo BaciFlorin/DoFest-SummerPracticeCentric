a86e64e7fcd26064038a39726ef7a8b0b3631a5b
sgabriel190 <strilciuc_gabriel98@yahoo.ro>
2020-08-09 15:42:06 +0300

    Integration tests.

---
+++ DoFest/DoFest.IntegrationTests/IntegrationTests.cs
@@ -0,0 +1,34 @@
+﻿using DoFest.API;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Moq;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DoFest.IntegrationTests
+{
+    public class IntegrationTests : IAsyncLifetime
+    {
+        private readonly WebApplicationFactory<Startup> webApplicationFactory;
+
+        protected HttpClient HttpClient { get; private set; }
+
+        protected string AuthenticationToken { get; private set; }
+
+        protected Guid AuthenticatedUserId { get; private set; }
+
+        // Adaugarea Loggerului
+        // protected Mock<IDomainLogger> MockLogger { get; private set; }
+
+        public Task DisposeAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task InitializeAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
