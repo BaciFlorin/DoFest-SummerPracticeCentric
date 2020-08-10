93632040c8df9e5fcab9d12035a1712e0c3edc85
sgabriel190 <strilciuc_gabriel98@yahoo.ro>
2020-08-10 12:21:36 +0300

    Add more tests to CommentsControllerTests.

--- DoFest/DoFest.IntegrationTests/CommentsControllerTests.cs
+++ DoFest/DoFest.IntegrationTests/CommentsControllerTests.cs
@@ -2,8 +2,10 @@
 using DoFest.Entities.Activities;
 using DoFest.Entities.Activities.Content;
 using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Xunit;
@@ -42,6 +44,89 @@ namespace DoFest.IntegrationTests
             response.IsSuccessStatusCode.Should().BeTrue();
             var comments = await response.Content.ReadAsAsync<IList<CommentModel>>();
             comments.Should().HaveCount(1);
+        }
+
+        [Fact]
+        public async Task AddCommentsToActivity()
+        {
+            // Arrange
+            var activity = new Activity(
+                Guid.NewGuid(),
+                Guid.NewGuid(),
+                "Nume activitate",
+                "Adresa activitate",
+                "Descriere activitate"
+                );
+            await ExecuteDatabaseAction(async (doFestContext) =>
+            {
+                await doFestContext.Activities.AddAsync(activity);
+                await doFestContext.SaveChangesAsync();
+            });
+            var newCommentModel = new NewCommentModel()
+            {
+                Content = "commentariu",
+                UserId = AuthenticatedUserId
+            };
+
+            // Act
+            var response = await HttpClient.PostAsJsonAsync($"api/v1/activities/{activity.Id}/comments", newCommentModel);
+
+            // Assert
+            response.IsSuccessStatusCode.Should().BeTrue();
+            var createdCommentId = response.Headers.Location.OriginalString;
+            Comment existingComment = null;
+            await ExecuteDatabaseAction(async (doFestContext) => {
+                var existingActvity = await doFestContext
+                                        .Activities
+                                        .Include(entity => entity.Comments)
+                                        .FirstAsync(entity => entity.Id ==activity.Id);
+
+                existingComment = existingActvity.Comments.FirstOrDefault();
+            });
+            existingComment.Should().NotBeNull();
+            existingComment.Id.Should().Be(createdCommentId);
+        }
+
+        [Fact]
+        public async Task DeleteCommentFromActivity()
+        {
+            // Arrange
+            var activity = new Activity(
+                Guid.NewGuid(),
+                Guid.NewGuid(),
+                "Nume activitate",
+                "Adresa activitate",
+                "Descriere activitate"
+                );
+            var comment = new Comment(
+                activity.Id,
+                this.AuthenticatedUserId
+                );
+            activity.AddComment(comment);
+            await ExecuteDatabaseAction(async (doFestContext) =>
+            {
+                await doFestContext.Activities.AddAsync(activity);
+                await doFestContext.SaveChangesAsync();
+            });
+
+
+            // Act
+            var response = await HttpClient.DeleteAsync($"api/v1/activities/{activity.Id}/comments/{comment.Id}");
+
+            // Assert
+            response.IsSuccessStatusCode.Should().BeTrue();
+            Activity existingActvity = null;
+            Comment existingComment = null;
+            await ExecuteDatabaseAction(async (doFestContext) =>
+            {
+                existingActvity = await doFestContext
+                                        .Activities
+                                        .Include(entity => entity.Comments)
+                                        .FirstAsync(entity => entity.Id == activity.Id);
+                existingComment = existingActvity.Comments.FirstOrDefault();
+            });
+            existingActvity.Comments.Should().HaveCount(0);
+            existingComment.Should().BeNull();
         }
     }
 }
