b9da03689a5e764c79396f722014528758fef886
sgabriel190 <strilciuc_gabriel98@yahoo.ro>
2020-08-11 11:58:02 +0300

    Add AutomatedTests

---
+++ DoFest/DoFest.AutomationTests/PageObjects/Dashboard/DashboardPage.cs
@@ -0,0 +1,18 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+using System;
+
+namespace DoFest.AutomationTests.PageObjects.Dashboard
+{
+    public class DashboardPage : BasePage
+    {
+        public DashboardPage(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(this, new RetryingElementLocator(this.driver, TimeSpan.FromSeconds(20)));
+        }
+
+        [FindsBy(How = How.ClassName, Using = "container")]
+        public IWebElement ContainerDashboard { get; set; }
+    }
+}
---
+++ DoFest/DoFest.AutomationTests/Tests/LoginTests.cs
@@ -0,0 +1,34 @@
+﻿using DoFest.AutomationTests.Helpers;
+using DoFest.AutomationTests.PageObjects.Dashboard;
+using DoFest.AutomationTests.PageObjects.Login;
+using System;
+using Xunit;
+
+namespace DoFest.AutomationTests.Tests
+{
+    public class LoginTests : Browser, IDisposable
+    {
+        public LoginPage loginPage;
+        public DashboardPage dashboardPage;
+
+        public LoginTests(): base()
+        {
+            Driver.Navigate().GoToUrl("http://localhost:4200/authentication");
+            loginPage = new LoginPage(Driver);
+        }
+
+        [Fact]
+        public void Login_With_Valid_Credentials()
+        {
+            loginPage.Login("DoFestAdmin@gmail.com", "passwordAdmin");
+            dashboardPage = new DashboardPage(Driver);
+            dashboardPage.WaitForPageToLoad("[label='Your bucketlist']");
+            Assert.True(dashboardPage.ContainerDashboard.Displayed);
+        }
+
+        public void Dispose()
+        {
+            CloseBrowser();
+        }
+    }
+}
